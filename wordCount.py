13ec787aa74ea92373720a26031c9176d8742e1e
Lester Ibarra <llibarra2@miners.utep.edu>
2020-09-02 23:48:42 -0600

    finished

--- wordCount.py
+++ wordCount.py
@@ -7,42 +7,43 @@ def usage():
     print(f"Usage: {progName} inputFile outputFile", file=stderr)
     
 try:
-    inFileName = argv[1]
-    outFileName = argv[2]
+    inFileName = argv[1] #takes input file
+    outFileName = argv[2] #takes output file
     
 except:
-    usage()
+    usage()#no input and/or output files submitted
     exit(1)
     
-def tryOpen(name, mode):
+def tryOpen(name, mode):#opens file
     try:
-        return open(name, mode)
+        return open(name, mode)#valid file
     except:
-        print(f"{progName}: Can't open file '{name}' in mode '{mode}'", file=stderr)
+        print(f"{progName}: Can't open file '{name}' in mode '{mode}'", file=stderr)#invalid file
         exit(1)
         
-inFile = tryOpen(inFileName, "r")
-outFile = tryOpen (outFileName, "w")
+inFile = tryOpen(inFileName, "r")#reads input file
+outFile = tryOpen (outFileName, "w")#writes on output file
 
-wordCounts = {}
+wordCounts = {}#keeps track of all words in input file
 
-for line in inFile.readLines():
+for line in inFile.readLines():#traverses each line on input file
     line = line +" "
     line = line.lower()
     line = sub(" [-.,:;'\n\t]"," ", line)
     line = sub(" +", " ", line)
     line = line[0: -1]
     
-    if len(line)==0:
+    if len(line)==0:# skips white space
         continue
-    for word in split (" ", line):
-        if word in wordCounts:
-            wordCounts[word] += 1
+    for word in split (" ", line):#splits line into words
+        if word not in wordCounts:
+            wordCounts[word] = 1 #new word found 
         else:
-            wordCounts[word]=1
+            wordCounts[word]=+1 #appearance of an already found word
             
 for word in sorted (wordCounts.keys()):
-    print(f"{word} {wordCounts[word]}", file = outFile)
+    print(f"{word} {wordCounts[word]}", file = outFile)#displays all words and counts for each
 
+#closes files
 inFile.close()
 outFile.close()
